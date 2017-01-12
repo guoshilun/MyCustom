c81c49345b71f79ff9442edd119f86b6ad8eadfb
allen <272255318@qq.com>
2017-01-12 16:33:38 +0800

    Initial commit

---
+++ mycustom2/src/main/java/com/example/mycustom2/CustomImageView.java
@@ -0,0 +1,180 @@
+package com.example.mycustom2;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * Created by ALun on 2017/1/6.
+ */
+
+public class CustomImageView extends View {
+    /**
+     * 控件的宽
+     */
+    private int mWidth;
+    /**
+     * 控件的高
+     */
+    private int mHeight;
+    /**
+     * 控件中的图片
+     */
+    private Bitmap mImage;
+    /**
+     * 图片的缩放模式
+     */
+    private int mImageScale;
+    private static final int IMAGE_SCALE_FITXY = 0;
+    private static final int IMAGE_SCALE_CENTER = 1;
+    /**
+     * 图片的介绍
+     */
+    private String mTitle;
+    /**
+     * 字体的颜色
+     */
+    private int mTextColor;
+    /**
+     * 字体的大小
+     */
+    private int mTextSize;
+
+    private Paint mPaint;
+    /**
+     * 对文本的约束
+     */
+    private Rect mTextBound;
+    /**
+     * 控制整体布局
+     */
+    private Rect rect;
+
+    //    public CustomImageView(Context context) {
+//        super(context);
+//    }
+//
+//    public CustomImageView(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//    }
+    public CustomImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomImageView(Context context) {
+        this(context, null);
+    }
+
+    public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomImageView, defStyleAttr, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.CustomImageView_image:
+                    mImage = BitmapFactory.decodeResource(getResources(), a.getResourceId(attr, 0));
+                    break;
+                case R.styleable.CustomImageView_imageScaleType:
+                    mImageScale = a.getInt(attr, 0);
+                    break;
+                case R.styleable.CustomImageView_titleText:
+                    mTitle = a.getString(attr);
+                    break;
+                case R.styleable.CustomImageView_titleTextColor:
+                    mTextColor = a.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.CustomImageView_titleTextSize:
+                    mTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+            }
+        }
+        a.recycle();
+        rect = new Rect();
+        mPaint = new Paint();
+        mTextBound = new Rect();
+        mPaint.setTextSize(mTextSize);
+        mPaint.getTextBounds(mTitle, 0, mTitle.length(), mTextBound);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //设置宽度
+        int specMode = MeasureSpec.getMode(widthMeasureSpec);
+        int specSize = MeasureSpec.getSize(widthMeasureSpec);
+        if (specMode == MeasureSpec.EXACTLY) {
+            mWidth = specSize;
+        } else {
+            //有图片决定宽
+            int desireByImg = getPaddingLeft() + getPaddingRight() + mImage.getWidth();
+            int desireByTitle = getPaddingRight() + getPaddingLeft() + mTextBound.width();
+            if (specMode == MeasureSpec.AT_MOST) {
+                int desir = Math.max(desireByImg, desireByTitle);
+                mWidth = Math.min(desir, specSize);
+            }
+        }
+        specMode = MeasureSpec.getMode(heightMeasureSpec);
+        specSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (specMode == MeasureSpec.EXACTLY)// match_parent , accurate  
+        {
+            mHeight = specSize;
+        } else {
+            int desire = getPaddingTop() + getPaddingBottom() + mImage.getHeight() + mTextBound.height();
+            if (specMode == MeasureSpec.AT_MOST)// wrap_content  
+            {
+                mHeight = Math.min(desire, specSize);
+            }
+        }
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        /**
+         * 边框
+         */
+        mPaint.setStrokeWidth(4);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.CYAN);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+        rect.left = getPaddingLeft();
+        rect.right = mWidth - getPaddingRight();
+        rect.top = getPaddingTop();
+        rect.bottom = mHeight - getPaddingBottom();
+
+        mPaint.setColor(mTextColor);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        if (mTextBound.width() > mWidth) {
+            TextPaint paint = new TextPaint(mPaint);
+            String msg = TextUtils.ellipsize(mTitle, paint, (float) mWidth - getPaddingLeft() - getPaddingRight(), TextUtils.TruncateAt.END).toString();
+            canvas.drawText(msg, getPaddingLeft(), mHeight - getPaddingBottom(), paint);
+        } else {
+            canvas.drawText(mTitle, mWidth / 2 - mTextBound.width() * 1.0f / 2, mHeight - getPaddingBottom(), mPaint);
+        }
+//取消使用中间的块
+        rect.bottom -= mTextBound.height();
+
+        if (mImageScale == IMAGE_SCALE_FITXY) {
+            canvas.drawBitmap(mImage, null, rect, mPaint);
+        } else {
+            //计算居中的矩形范围
+            rect.left = mWidth / 2 - mImage.getWidth() / 2;
+            rect.right = mWidth / 2 + mImage.getWidth() / 2;
+            rect.top = (mHeight - mTextBound.height()) / 2 - mImage.getHeight() / 2;
+            rect.bottom = (mHeight - mTextBound.height()) / 2 + mImage.getHeight() / 2;
+
+            canvas.drawBitmap(mImage, null, rect, mPaint);
+        }
+    }
+}
---
+++ mycustom3/src/main/java/com/example/mycustom3/MyCustom3.java
@@ -0,0 +1,127 @@
+package com.example.mycustom3;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * Created by ALun on 2017/1/6.
+ */
+
+public class MyCustom3 extends View {
+    /**
+     * 第一圈的颜色
+     */
+    private int mFirstColor;
+    /**
+     * 第二圈的颜色
+     */
+    private int mSecondColor;
+    /**
+     * 圈的宽度
+     */
+    private int mCircleWidth;
+    /**
+     * 画笔
+     */
+    private Paint mPaint;
+    /**
+     * 当前进度
+     */
+    private int mProgress;
+
+    /**
+     * 速度
+     */
+    private int mSpeed;
+
+    /**
+     * 是否应该开始下一个
+     */
+    private boolean isNext = false;
+
+
+    public MyCustom3(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyCustom3(Context context) {
+        this(context, null);
+    }
+
+    public MyCustom3(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomProgressBar, defStyleAttr, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.CustomProgressBar_firstColor:
+                    mFirstColor = a.getColor(attr, Color.GREEN);
+                    break;
+                case R.styleable.CustomProgressBar_secondColor:
+                    mSecondColor = a.getColor(attr, Color.RED);
+                    break;
+                case R.styleable.CustomProgressBar_circleWidth:
+                    mCircleWidth = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
+                    break;
+                case R.styleable.CustomProgressBar_speed:
+                    mSpeed = a.getInt(attr, 20);
+                    break;
+            }
+        }
+        a.recycle();
+        mPaint = new Paint();
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    mProgress++;
+                    if (mProgress == 360) {
+                        mProgress = 0;
+                        if (isNext) {
+                            isNext = true;
+                        } else {
+                            isNext = false;
+                        }
+                    }
+                    postInvalidate();
+                    try {
+                        Thread.sleep(mSpeed);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int centre = getWidth() / 2;
+        int radius = centre - mCircleWidth / 2;
+        mPaint.setStrokeWidth(mCircleWidth);
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        RectF oval = new RectF(centre - radius, centre - radius, centre + radius, centre + radius);
+        if (!isNext) {
+            mPaint.setColor(mFirstColor);
+            canvas.drawCircle(centre, centre, radius, mPaint);
+            mPaint.setColor(mSecondColor);
+            canvas.drawArc(oval, -90, mProgress, false, mPaint);
+        } else {
+            mPaint.setColor(mSecondColor);
+            canvas.drawCircle(centre, centre, radius, mPaint);
+            mPaint.setColor(mFirstColor); // 设置圆环的颜色
+            canvas.drawArc(oval, -90, mProgress, false, mPaint);
+        }
+    }
+}
